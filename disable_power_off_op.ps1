6357b94b4d46a17400ff6143b89e63844182146c
netikus <84867531+netikus@users.noreply.github.com>
2021-06-26 11:04:48 -0500

    Removed extra new line character

--- disable_power_off_op.ps1
+++ disable_power_off_op.ps1
@@ -1,4 +1,3 @@
-# This script will disable the power off option from the start menu on Windows. Useful for virtual machines or remote access where it may be difficult to power the machine back on
+# Disables the power off option from the start menu on Windows. Useful for virtual machines or remote access where it may be difficult to power the machine back on
 
-New-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\Start\HideShutDown\ -Name Value -Value 1 
--PropertyType DWORD -Force
+New-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\Start\HideShutDown\ -Name Value -Value 1 -PropertyType DWORD -Force
