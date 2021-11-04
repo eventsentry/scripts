8e5d01e16838c8ee7285c9bb92bb4accbd474f6e
netikus <84867531+netikus@users.noreply.github.com>
2021-11-04 11:13:45 -0500

    Improved comments

--- crypto_to_usd.ps1
+++ crypto_to_usd.ps1
@@ -1,5 +1,10 @@
 # Converts the price of a crypto currency to USD, using the messari.io API
 # Please note that the messari API enforces rate limits, see https://messari.io/api/docs
+#
+# Pass symbol name of crypto as the argument, e.g. BTC
+#
+# This script can easily be integrated with EventSentry's performance monitoring functionality (option "executable"), e.g.
+# powershell.exe -inputformat none -executionpolicy bypass -file crypto_to_usd.ps1 BTC
 
 $coinID = $args[0]
 
