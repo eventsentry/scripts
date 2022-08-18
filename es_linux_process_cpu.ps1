b41e817e5fdd7a7af51777058dc3816444d351eb
netikus <84867531+netikus@users.noreply.github.com>
2022-08-17 22:40:10 -0500

    Initial commit

---
+++ es_linux_process_cpu.ps1
@@ -0,0 +1,81 @@
+# This script can easily be integrated with EventSentry's performance monitoring functionality (option "executable")
+# See KB 473 for more information
+
+If ($args.Length -le 0)
+{
+	Write-Host "Hostname argument required"
+	Exit(1)
+}
+
+# Path to the plink utility
+$pathToPlink = "c:\tools\ssh\plink.exe"
+
+# User for SSH login
+$sshUser = "ingmar"
+
+# Path to SSH public key cert, user/pass auth can be used an alternative
+$sshPathToCert = "C:\tools\ssh\gentoo"
+
+$procInstance = @{}
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.Arguments = '-batch -i ' + $sshPathToCert + ' -l ' + $sshUser + ' ' + $args[0] + ' "ps -eo %cpu,cmd"'
+$pinfo.FileName = $pathToPlink
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.CreateNoWindow = $true
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+
+$stdout = $p.StandardOutput.ReadToEnd()
+$stderr = $p.StandardError.ReadToEnd()
+
+$processes = @()
+$cpuValues = @()
+
+$lines = $stdout.Split("`n")
+
+ForEach ($line in $lines)
+{
+    $tokens = $line.Split(" ")
+       
+    If ($tokens.Count -gt 2)
+    {
+        $cpuUsage = $tokens[1]
+        $processName = $tokens[2]
+        
+        $processName = $processName.TrimEnd(":")
+        $processName = $processName.TrimEnd(")")
+        $processName = $processName.TrimStart("-")
+        $processName = $processName.TrimStart("(")
+        
+        $processNameOutput = $processName
+        
+        If ($procInstance[$processName] -gt 0)
+        { 
+            $processNameOutput += "#"
+            $processNameOutput += $procInstance[$processName] 
+        }
+        
+        $processes += $processNameOutput
+        $cpuValues += [Math]::Round($cpuUsage)
+                
+        If ($procInstance[$processName] -eq '')
+            { $procInstance[$processName] = 1 }
+        Else
+            { $procInstance[$processName]++ }
+    }
+}
+
+ForEach ($x in $processes)
+{
+	Write-host "$x," -NoNewLine
+}
+Write-host ""
+
+ForEach ($x in $cpuValues)
+{
+	Write-host "$x," -NoNewLine
+}
